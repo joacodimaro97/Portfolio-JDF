43a54da933b3baa11acb5f9ae8a7d52121c5a139
Joaquin Dimaro <joacodimaro97@gmail.com>
2023-06-24 20:32:06 -0300

    Vista inicio

---
+++ src/router/Router.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import App from "../App";
+import  Index  from "../pages/Index";
+import 'tailwindcss/tailwind.css';
+ 
+export default function Router(){
+
+    return(
+        <Routes>
+            <Route element={<App/>}>
+                <Route path="/" element={<Index/>}/>
+            </Route>
+        </Routes>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens:{
+        'mob': {'min': '320px', 'max': '639px'},
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
+
