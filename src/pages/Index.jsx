68e44abfeff2bc364f3f3a69bc61b8be727b58f3
Joaquin Dimaro <joacodimaro97@gmail.com>
2023-06-27 16:00:25 -0300

    Main (#3)
    * Vista inicio
    
    * postimage
    
    * Index responsive && skills
    
    * carousel proyects

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
-
+import Card from '../components/Card';
 
 export default function Index(){
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -19,7 +19,6 @@ export default function Index(){
 
   return (
     <>
-
     <div className='w-full min-h-[200vh] flex flex-col'>
     <div className=' bg-[url("https://i.postimg.cc/kMH7GJDM/gabriel-heinzer-EUzk9-BIEq6-M-unsplash.jpg")] pt-20 bg-cover bg-center bg-no-repeat bg-black  bg-fixed w-full h-[100vh] flex flex-col justify-end items-center'>
         
@@ -42,6 +41,7 @@ export default function Index(){
 
         </div>
         </div>
+    
         <div className=' absolute   md:w-[10vw] h-[60vh] md:right-0 md:top-40'> 
         <div className='flex  md:flex-col h-full justify-between items-center'>
         <svg  fill="#ffffff" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="64px" height="64px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="c133de6af664cd4f011a55de6b000d0b"> <path display="inline" d="M108.669,0.501h23.032v22.756h21.069V0.501h23.034V69.41h-23.032V46.334h-21.069V69.41h-23.032V0.501 H108.669z M206.091,23.353h-20.275V0.501h63.594v22.852h-20.285V69.41h-23.032V23.353H206.091z M259.502,0.501h24.02l14.771,24.213 l14.759-24.213h24.023V69.41h-22.938V35.256l-15.845,24.5h-0.395l-15.856-24.5V69.41h-22.539V0.501z M348.54,0.501h23.038v46.133 h32.391V69.41H348.54V0.501z M74.987,100.926l32.946,369.533l147.844,41.04L404.031,470.4l32.981-369.475H74.987z M368.289,188.62 l-2.063,22.977l-0.906,10.188h-0.149H256h-0.158h-63.956l4.142,46.407h59.814H256h92.98h12.214l-1.106,12.172l-10.65,119.32 l-0.682,7.652L256,433.045v0.008l-0.208,0.059l-92.839-25.774l-6.351-71.161h20.97h24.527l3.227,36.146l50.474,13.632l0.042-0.013 v-0.004l50.551-13.64l5.257-58.781H256h-0.158H154.578L143.439,188.62l-1.085-12.157h113.488H256h113.374L368.289,188.62z"> </path> </g> </g></svg>  
@@ -52,11 +52,10 @@ export default function Index(){
         </div>
         </div>
     </div>
-
     <div className='w-full min-h-[80vh] bg-[#fff] flex flex-col items-center md:flex-row'>
     <div className='  m-4 md:m-0  md:w-[50%] flex items-center justify-center h-full '>
     
-     <img src="https://i.postimg.cc/W3yXGkc4/image2.png" className=' w-[80%] h-[68%]' alt="" />
+     <img src="https://i.postimg.cc/W3yXGkc4/image2.png" className='shadow-md shadow-black w-[80%] h-[68%]' alt="" />
      
     </div>
      <div className='w-[90%] md:w-[50%] h-full  items-center justify-evenly flex flex-col '>
@@ -98,12 +97,14 @@ export default function Index(){
       </div>
     </div>
     </div>
-    <div className='w-full h-[80vh] bg-white flex'>
+    <div className='w-full min-h-[80vh] bg-white flex flex-col'>
+      <h1 className='font-montserrat text-[45px] font-black tracking-[10px] border-l-[8px] border-[#ffe83c] w-[35vh] h-[20vh] ml-2 mt-2 pl-4 ' >My projects</h1>
+       <div className='w-full min-h-[90vh]  flex items-center justify-center '>
+            <Card/>
+       </div>
 
     </div>
     </div>
-
-
    
     </>
   )
